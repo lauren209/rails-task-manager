1617634b41dbd6340f56be8bc1a53f87d03b1759
Lauren DeFrancesco <lauren209@live.com>
2017-06-06 11:55:34 +0200

    made forms and the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  get 'tasks', to: "tasks#index"
+
+  get 'tasks/new', to: "tasks#new"
+
+  get 'tasks/:id', to: "tasks#show"
+  post 'tasks', to: "tasks#create"
+
+  get 'tasks/edit', to: "tasks#edit"
+  patch 'tasks/update', to: "tasks#update"
+  delete 'tasks/destroy', to: "tasks#destroy"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
